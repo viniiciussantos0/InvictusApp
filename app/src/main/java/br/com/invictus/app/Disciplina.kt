ed49336513fa911bf5c7cee2563b10e377f9a3b5
lucas-sesti <lucas0072011sp@hotmail.com>
2020-10-26 16:29:09 -0300

    iniciando ac3

---
+++ app/src/main/java/br/com/invictus/app/Disciplina.kt
@@ -0,0 +1,32 @@
+package br.com.invictus.app
+
+import android.content.Context
+
+class Disciplina {
+
+    var id:Long = 0
+    var nome = ""
+    var ementa = ""
+    var foto = ""
+    var professor = ""
+
+    override fun toString(): String {
+        return "Disciplina(nome='$nome')"
+    }
+}
+
+object DisciplinaService {
+    fun getDisciplinas (context: Context): List<Disciplina> {
+        val disciplinas = mutableListOf<Disciplina>()
+// criar 10 disciplinas
+        for (i in 1..10) {
+            val d = Disciplina()
+            d.nome = "Disciplina $i"
+            d.ementa = "Ementa Disciplina $i"
+            d.professor = "Professor Disciplina $i"
+            d.foto = "https://dummyimage.com/600x400/000/fff"
+            disciplinas.add(d)
+        }
+        return disciplinas
+    }
+}
--- app/src/main/java/br/com/invictus/app/TelaInicialActivity.kt
+++ app/src/main/java/br/com/invictus/app/TelaInicialActivity.kt
@@ -1,4 +1,4 @@
-package br.com.invictus.app
+    package br.com.invictus.app
 
 import android.content.Context
 import android.content.Intent
@@ -9,16 +9,23 @@ import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.gson.Gson
 import com.google.gson.internal.LinkedTreeMap
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_tela_inicial.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class TelaInicialActivity : DebugActivity() {
-
+class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private val context: Context get() = this
+    private var disciplinas = listOf<Disciplina>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
+        configuraMenuLateral()
 
         setSupportActionBar(toolbar_view)
 
@@ -26,6 +33,40 @@ class TelaInicialActivity : DebugActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         loadItems()
+    }
+
+    private fun configuraMenuLateral() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            layoutMenuLateral,
+            toolbar_view,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        layoutMenuLateral.addDrawerListener(toggle)
+        toggle.syncState()
+        menu_lateral.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.adicionar_menu -> {
+                startActivity(Intent(this, AdicionarActivity::class.java))
+            }
+            R.id.nav_mensagens -> {
+                Toast.makeText(this, "Clicou Mensagens", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_forum -> {
+                Toast.makeText(this, "Clicou Forum", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_config -> {
+                Toast.makeText(this, "Clicou Config", Toast.LENGTH_SHORT).show()
+            }
+        }
+        // fecha menu depois de tratar o evento
+        layoutMenuLateral.closeDrawer(GravityCompat.START)
+        return true
     }
 
     fun loadItems() {
@@ -47,27 +88,27 @@ class TelaInicialActivity : DebugActivity() {
             R.layout.listview_item, listItems
         )
 
-        val listView: ListView = findViewById(R.id.itens_produtos)
-        listView.adapter = adapter
-
-        listView.onItemClickListener =
-            OnItemClickListener { parent, view, position, id ->
-                val sharedPref = getSharedPreferences("PRODUTOS", Context.MODE_PRIVATE)
-                val gson = Gson()
-                val arrayType = object : TypeToken<Array<Any>>() {}.type
-
-                val items: Array<Any> =
-                    gson.fromJson(sharedPref.getString("produtos", "[]"), arrayType)
-
-                val intent = Intent(
-                    this,
-                    ProdutoActivity::class.java
-                )
-
-                intent.putExtra("item_produto", position)
-
-                startActivity(intent)
-            }
+//        val listView: ListView = findViewById(R.id.itens_produtos)
+//        listView.adapter = adapter
+//
+//        listView.onItemClickListener =
+//            OnItemClickListener { parent, view, position, id ->
+//                val sharedPref = getSharedPreferences("PRODUTOS", Context.MODE_PRIVATE)
+//                val gson = Gson()
+//                val arrayType = object : TypeToken<Array<Any>>() {}.type
+//
+//                val items: Array<Any> =
+//                    gson.fromJson(sharedPref.getString("produtos", "[]"), arrayType)
+//
+//                val intent = Intent(
+//                    this,
+//                    ProdutoActivity::class.java
+//                )
+//
+//                intent.putExtra("item_produto", position)
+//
+//                startActivity(intent)
+//            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
